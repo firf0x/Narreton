6613b1cc65dcadee44ed5c20688a795b4ebd2c5b
firf0x <hololsw@gmail.com>
2024-06-10 16:47:49 +0800

    patch 1

--- Assets/Resources/Scripts/Camera/cameraController.cs
+++ Assets/Resources/Scripts/Camera/cameraController.cs
@@ -1,8 +1,31 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using System;
 
-public class cameraController : MonoBehaviour {
-    
-    
+namespace Assets.Resources.Scripts.Camera
+{
+    public class cameraController : MonoBehaviour{
+        private MainController controller;
+        public PlayerInput PlayerInput;
+        private Vector2 cameraInput;
+        private Rigidbody2D _rb;
 
+        [SerializeField] private float Speed;
 
+        private void Awake() {
+            controller = new MainController();
+
+            controller.Enable();
+
+            _rb = gameObject.GetComponent<Rigidbody2D>();
+            if(PlayerInput == null)
+                Debug.LogError("PlayerInput пуст");
+        }
+
+        private void FixedUpdate() {
+            
+            cameraInput = controller.Move.Movement.ReadValue<Vector2>();
+            _rb.MovePosition(_rb.position + (cameraInput * Speed * Time.fixedDeltaTime));
+        }
+    }
 }
